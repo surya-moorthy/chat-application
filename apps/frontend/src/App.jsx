5b2aeef4a52bdc4a0acf704133f2a43ba662fb37
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2025-05-08 07:35:47 +0530

    frontend added

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import  { BrowserRouter,Route, Routes } from "react-router-dom";
 import Chat from "./pages/Chat";
+import Chatrewrite from "./pages/Chatrewite";
 
 function App(){
   return (
     <BrowserRouter>
     <Routes>
-       <Route path="/chat" element={Chat}/>
+       <Route path="/chat" element={<Chat/>}/>
+       <Route path="/chatroom" element={<Chatrewrite/>}/>
+
     </Routes>
     </BrowserRouter>
   )
